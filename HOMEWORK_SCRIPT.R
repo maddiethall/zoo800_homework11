08cc630df132c6d87d9b6c6bd642149395976536
maddiethall <methall@wisc.edu>
2025-11-14 15:24:50 -0600

    finished!

--- HOMEWORK_SCRIPT.R
+++ HOMEWORK_SCRIPT.R
@@ -32,8 +32,6 @@ monkey_df = data.frame(
   meanDailyDistance_km
 )
 
-# write.csv(monkey_df, "monkey_travel_data.csv", row.names = FALSE)
-
 #### Scenario:
 # Primate habitats are often fragmented due to anthropogenic pressures
 # such as deforestation and land-use change. Researchers are interested
@@ -51,47 +49,48 @@ monkey_df = data.frame(
 #### to changes in mean daily temperature in terms of their daily travel distance?
 
 
-# OBJECTIVE 2
+########### OBJECTIVE 2 ########################################################
 
 # Scenario given to me by Sophia Mummert:
 # The chacma baboons at NVBP faces ecological pressure in the winter season
 # in nutrition available in their Afrotemperate habitat. With this seasonal
 # flux in nutrient availability, they face changes to activity budgets, seeing
 # a decrease in activities such as resting. This is due to an
-# increased amount of time spent on foraging.
+# increased amount of time spent on foraging, as their preferred foods are not
+# available. 
 
-# Does the chacma baboon's time spent resting face a difference in 
-# seasonal time spent foraging?
+# Does the chacma baboon's face a seasonal difference in time spent resting
+# in response to the time spent foraging?
 
-baboon_df = read.csv("baboon_sim_data (1).csv")
+# Two-level factor (Season): Summer vs. Winter 
+# Continuous predictor: Mean time spent foraging in hours (weekly)
+# Response Variable: Mean time spent resting in hours (weekly)
 
-## Two-level factor: group ("summer" and "winter")
-## Continuous predictor: time_spent_foraging
-## Response variable: time_spent_resting
+baboon_df = read.csv("baboon_sim_data (2).csv")
 
 mod1 = lm(time_spent_resting ~ time_spent_foraging * group, data = baboon_df)
 summary(mod1)
-# significant interaction effect between season and foraging time
-# significant effect of season on resting time
-# significant effect of foraging time on resting time
+# significant interaction effect between season and foraging time (p < 0.001)
+# significant effect of season on resting time (p = 0.03)
+# significant effect of foraging time on resting time (p < 0.001)
 #### parameter estimates:
-#### beta_0 = 7.26; beta_1 = -0.05; beta_2 = 2.72; beta_3 = -0.097
+#### beta_0 = 8; beta_1 = -0.06; beta_2 = 2.14; beta_3 = -0.09
+
+mod2 = lm(time_spent_resting ~ time_spent_foraging + group, data = baboon_df)
+summary(mod2)
+# removing the interaction effect from the model decreases the fit
 
 ggplot(baboon_df, aes(x = time_spent_foraging, y = time_spent_resting, color = group)) +
   geom_smooth(method = "lm", se = TRUE) +
   labs(
     title = "Seasonal Effect of Time Spent Resting vs Foraging",
-    x = "Time Spent Foraging",
-    y = "Time Spent Resting"
+    x = "Mean Weekly Foraging Time (hours)",
+    y = "Mean Weekly Resting Time (hours)"
   ) +
   theme_minimal()
 # different slopes for summer and winter groups, both negative correlation
 # steeper slope for winter group, indicating a more severe effect of foraging time on resting time
 # compared to the summer group
-
-mod2 = lm(time_spent_resting ~ time_spent_foraging + group, data = baboon_df)
-summary(mod2)
-# removing the interaction effect from the model decreases the fit
 
 
 #### RESULTS EXPLAINATION ####
@@ -107,4 +106,6 @@ summary(mod2)
 # time is influenced by seasonal changes, likely due to the varying availability of food resources.
 
 #### Comparison to the truth:
-
+## true parameters: beta_0 = 6; beta_1 = -0.05; beta_2 = 3; beta_3 = -0.1
+# My estimates are fairly close to the true parameters,
+# and Sophia confirmed that my interpretation of the results is correct!
